6cbba175a263d90b46dd312716a38eaa7d30bcd3
Gangadhar <naatarugangadhar@gmail.com>
2025-03-11 17:53:18 +0530

    txt extraction from docx flie

---
+++ Txt_Ext_docx.py
@@ -0,0 +1,14 @@
+from docx import Document  
+
+def extract_txt_from_docx(path):
+  doc=Document(path)
+  txt=[]
+   
+  for para in doc.paragraphs:
+      txt.append(para.text)
+
+  ext_txt="\n".join(txt).strip() 
+  return ext_txt
+
+doc_path="PUNITH SURE INTERNSHIP CV (1).docx"
+print(extract_txt_from_docx(doc_path))
