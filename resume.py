4bd9bfc4ba618b11730ecb27055b3d8fc6cbc424
reddykrishna <donthureddykrishna43@gmail.com>
2025-03-12 13:57:37 +0530

    pdf text extraction code

--- resume.py
+++ resume.py
@@ -1,21 +1,19 @@
-import fitz 
+import fitz  
 
 def extract_text(pdf_path):
     pdf = fitz.open(pdf_path)
-    
+    extracted_text = ""  
 
     for i, page in enumerate(pdf, start=1):
-        print(f"Page {i}") 
+        extracted_text += f"Page {i}\n"
         blocks = page.get_text("blocks")
-        blocks.sort(key=lambda b: (b[1], b[0]))
+        blocks.sort(key=lambda b: (b[1], b[0]))  
 
         for block in blocks:
-            print(block[4])  
-            print("----")
-          
+            extracted_text += block[4] + "\n----\n"  
 
-    
+    return extracted_text  
 
 pdf_path = "My cv.pdf"  
 text = extract_text(pdf_path)
-print(text)
+print(text)  
